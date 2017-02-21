c0473e8683d47bcb44d9d408c85d90ea4c631164
Hoon Cho <Hoon Cho>
2017-02-22 00:58:52 +0900

    bower wiredep setting done

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,54 @@ gulp.task('style', function(){
 		}))
 		.pipe(jscs());
 });
+
+gulp.task('inject',function () {
+
+    var wiredep=require('wiredep').stream;
+
+    var Promise = require('any-promise');
+
+    var inject=require('gulp-inject');
+
+    var injectSrc=gulp.src(['./public/css/*.css','./public/js/*.js'],{read:false});
+
+    var injectOptions={
+
+        ignorePath:'/public'
+
+    };
+
+    var options={
+
+        bowerJson:require('./bower.json'),
+
+        directory:'./public/lib',
+
+        ignorePath:'../../public'
+
+    };
+
+    return gulp.src('./src/views/*.html')
+
+    .pipe(wiredep(options))
+
+    .pipe(inject(injectSrc,injectOptions))  //wiredep and gulp-inject are used for inject  all bower packages,html and js files into index.html
+
+    .pipe(gulp.dest('./src/views'));
+
+});
+
+
+
+/*gulp.task('inject', function(){
+	var wiredep = require('wiredep').stream;
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib',
+		ignorePath: '../../public'
+	};
+
+	return gulp.src('./src/views/*.html')
+		.pipe(wiredep(options))
+		.pipe(gulp.dest('.src/views'));
+});*/
