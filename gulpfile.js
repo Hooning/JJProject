5b441e3d348b11c029ab2582efc2275f079cba25
Jiyeon Jeon <JiyeonJeon@Jiyeonui-MacBook-Air.local>
2017-02-22 09:03:47 +0900

    Final setting for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-
-
 
 gulp.task('style', function(){
 	return gulp.src(jsFiles)
@@ -26,9 +25,7 @@ gulp.task('inject',function () {
     var injectSrc=gulp.src(['./public/css/*.css','./public/js/*.js'],{read:false});
 
     var injectOptions={
-
         ignorePath:'/public'
-
     };
 
     var options={
@@ -48,20 +45,20 @@ gulp.task('inject',function () {
     .pipe(inject(injectSrc,injectOptions))  //wiredep and gulp-inject are used for inject  all bower packages,html and js files into index.html
 
     .pipe(gulp.dest('./src/views'));
-
 });
 
+gulp.task('serve', ['style','inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1, 
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
 
-
-/*gulp.task('inject', function(){
-	var wiredep = require('wiredep').stream;
-	var options = {
-		bowerJson: require('./bower.json'),
-		directory: './public/lib',
-		ignorePath: '../../public'
-	};
-
-	return gulp.src('./src/views/*.html')
-		.pipe(wiredep(options))
-		.pipe(gulp.dest('.src/views'));
-});*/
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting.....');
+        });
+});
